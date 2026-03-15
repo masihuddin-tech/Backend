a49d495c58fceb6a007700cf6c44f0260febdd97
masihuddin-tech <khanmasihuddin22@gmail.com>
2026-03-15 23:31:23 +0530

    Async done

--- JS/Async-await.js
+++ JS/Async-await.js
@@ -1,30 +1,47 @@
 async function github(){
-    // console.log("I am inside the async function");
-    const response = await fetch("https://api.github.com/users");
-    const data = await response.json();
-    // console.log(data);
-    
-    const parent = document.getElementById("first");
-    for(let user of data){      //Creating all users UI(Image,username,URL) using for loop
-        const div = document.createElement("div");
-        div.classList.add("user");
+    try{
+        // console.log("I am inside the async function");
 
-        const img = document.createElement("img");
-        img.src = user.avatar_url;
+        const response = await fetch("https://api.github.com/users");
+        if(!response.ok){
+            throw new Error("Data is not present");
+        }
 
-        const userName = document.createElement("h2");
-        userName.textContent = user.login;
 
-        const anchor = document.createElement("a");
-        anchor.href = user.html_url;
-        anchor.textContent = "Visit Profile";
+        const data = await response.json();
+        // console.log(data);
+        
+        const parent = document.getElementById("first");
+        for(let user of data){      //Creating all users UI(Image,username,URL) using for loop
+            const div = document.createElement("div");
+            div.classList.add("user");
 
-        div.append(img,userName,anchor);
-        parent.append(div);
+            const img = document.createElement("img");
+            img.src = user.avatar_url;
+
+            const userName = document.createElement("h2");
+            userName.textContent = user.login;
+
+            const anchor = document.createElement("a");
+            anchor.href = user.html_url;
+            anchor.textContent = "Visit Profile";
+
+            div.append(img,userName,anchor);
+            parent.append(div);
+        }
+    }
+    catch(error){
+        console.log("Error");
     }
 
 }
 github();
+
+
+// //This is the code to check the above github api usage. 
+fetch("https://api.github.com/rate_limit")
+  .then(r => r.json())
+  .then(d => console.log(d.rate));
+
 // console.log("I am outside the async function");
 
-// //31:55
